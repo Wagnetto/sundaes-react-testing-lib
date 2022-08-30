a2de02618e397c7825482eb0653a8386e50fe399
Wagner Silveira Souto <wagner.souto@ifood.com.br>
2022-08-30 19:14:20 -0300

    Mock service worker setup

--- src/pages/Summary/test/SummaryForm.test.js
+++ src/pages/Summary/test/SummaryForm.test.js
@@ -27,8 +27,27 @@ test("checkbox enables and disables the button", async () => {
   expect(button).toBeDisabled();
 });
 
-test("popover responds to hover", () => {
+test("popover responds to hover", async () => {
+  const user = userEvent.setup();
+  render(<SummaryForm />);
+
   //popover starts out hidden
+  const nullpopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(nullpopover).not.toBeInTheDocument();
+
   //popover appears upon mouseover of checkbox
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  await user.hover(termsAndConditions);
+
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+
   //popover desappears when we mouse out
+  await user.unhover(termsAndConditions);
+  const overlay = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(overlay).not.toBeInTheDocument();
 });
